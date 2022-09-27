4f1cdaad71ef4fe090b11459bf3cceb4fbfbcb31
aberguecio1 <48263883+aberguecio1@users.noreply.github.com>
2022-09-27 10:38:57 -0300

    working movement of 1 player

--- Assets/code/personaje.cs
+++ Assets/code/personaje.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class personaje : MonoBehaviour
 {
     private Rigidbody2D  Rigidbody;
-    private float Horizontal;
+    private Vector2 input_move;
     public float jump; /* al ser public las editamos en unity */
     public float speed;
-    private bool Grounded;
+    private bool grounded;
+    public bool Player1_in;
+    public bool Player2_in;
+    public bool Player3_in;
+    public bool Player4_in;
 
     void Start()
     {
@@ -17,27 +22,37 @@ public class personaje : MonoBehaviour
 
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal"); /* +1 con D y -1 con A */
-
-        /* Debug.DrawRay(transform.position, Vector3.down * 0.2f,Color.red); */ 
-        if (Physics2D.Raycast(transform.position, Vector3.down ,0.15f)){/* rallo para abajo para saber si hay terreno */
-            Grounded=true;
+        if (Physics2D.Raycast(transform.position,Vector2.down,0.2f)){
+            grounded=true;
         }else{
-            Grounded=false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && Grounded){
-            Jump();
+            grounded=false;
+            Debug.Log("Jump");
         }
     }
 
-    private void Jump(){
-        Rigidbody.AddForce(Vector2.up*jump);
+    public void Jump(InputAction.CallbackContext context){
+        if (context.performed & grounded & Player1_in){
+            Rigidbody.AddForce(Vector2.up*jump);
+        }
     }
+
+    public void Move(InputAction.CallbackContext context){
+        input_move = context.ReadValue<Vector2>();
+        Debug.Log(input_move);
+    }
+
+    public void Possess(InputAction.CallbackContext context){
+        if (context.performed ){
+            Player1_in = !Player1_in;
+        }
+    }
+
 
     void FixedUpdate()
     {
-        Rigidbody.velocity = new Vector2(Horizontal*speed,Rigidbody.velocity.y);
+        if (Player1_in){
+            Rigidbody.velocity = new Vector2(input_move.x*speed,Rigidbody.velocity.y);
+        }
     }
     
 }
--- Assets/code/player1.cs
+++ Assets/code/player1.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D  playerbody;
+    private Vector2 input_move;
+    public float speed;
+    public bool Player1_in;
+
+
     void Start()
     {
-        
+        playerbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+    public void Move(InputAction.CallbackContext context){
+        input_move = context.ReadValue<Vector2>();
+    }
+
+    public void Possess(InputAction.CallbackContext context){
+        if (context.performed ){
+            Player1_in = !Player1_in;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Player1_in){
+            playerbody.MovePosition(playerbody.position + input_move * speed);
+        }
+
+    }
+    
 }
